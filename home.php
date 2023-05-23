86683d813eeb0ade741aa6903e2d2286fb35d760
pierre.K <minder309@gmail.com>
2023-05-24 00:21:27 +0800

    首頁模板建置完成。差微調。

---
+++ home.php
@@ -0,0 +1,142 @@
+<!-- 旋轉木馬 -->
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+            <img src="./img/P1127690.JPG" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block text-white">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+            <img src="./img/P1127693.JPG" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block text-white">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="./img/P1127693-01.jpeg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block text-white">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+<!-- 卡片輪播測試。 -->
+<div id="carouselExampleControls" class="carousel slide carousel-dark" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <!-- 第一章卡片 -->
+        <div class="carousel-item active">
+            1
+            <div class="row mx-auto justify-content-center">
+                <div class="card col-lg-3 col-12">
+                    <img src="./img/P1127690.JPG" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-12">
+                    <img src="./img/P1127690.JPG" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-12">
+                    <img src="./img/P1127690.JPG" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- 第二張卡片 -->
+        <div class="carousel-item">
+            2
+            <div class="row mx-auto justify-content-center">
+                <div class="card col-lg-3 col-12">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-12">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-12">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- 第三張卡片 -->
+        <div class="carousel-item">
+            3
+            <div class="row mx-auto justify-content-center">
+                <div class="card col-lg-3 col-12">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-12">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-12">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
--- index.php
+++ index.php
@@ -15,78 +15,61 @@
 
 <body>
     <!-- 導覽bar -->
-    <nav class="navbar">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">澈方</a>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">澈方</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">首頁</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">品牌理念</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">魚缸系列</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">烏龜系列</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">聯絡資訊</a>
-                </li>
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="?do=home">
+                            回首頁
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            品牌理念
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            魚缸系列
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            烏龜系列
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            聯絡資訊
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
     <!-- container--xl指地的是在視窗達到1200xp前，都會是100%佔滿這個視窗。 -->
     <div class="container-xl">
-        <!-- 旋轉木馬 -->
-        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="./img/P1127690.JPG" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block text-white">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="./img/P1127693.JPG" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block text-white">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="./img/P1127693-01.jpeg" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block text-white">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
+        <?php
+        // 如果$_GET['do']存在，$do=$_GET['do']，如果不存在$do=home;
+        $do = $_GET['do'] ?? 'home';
+        // 將$do存入$file內，並且轉成資料夾位置。
+        $file = "./" . $do . ".php";
+        if (file_exists($file)) {
+            include $file;
+        } else {
+            include "./home.php";
+        }
+        ?>
+    </div>
 
-        <!-- 參考網站:https://www.adana.co.jp/en/ -->
-        <!-- 參考網站:https://www.onf.com.tw/ -->
-        <!--
+    <!-- 參考網站:https://www.adana.co.jp/en/ -->
+    <!-- 參考網站:https://www.onf.com.tw/ -->
+    <!--
     首頁 
     1.首頁左上角為mark，還有中文字。
     2.mark後面接英文名子，並且置中。
@@ -102,13 +85,21 @@
         5-2 用卡片做出，精選商品介紹。
     6.最下面寫聯絡資訊跟fb。
     -->
-        <!-- 後臺管理系統:
+    <!-- 後臺管理系統:
     1.可以寫新文案。
     2.可以可以上傳圖片。
     3.登入系統。
     4.改密碼葉面。
     -->
     </div>
+    <footer class="container-xl">
+        <div class="position-absolute col-4 end-0">
+            <a href="#" style="text-decoration:none;color:black;">© 澈方 2023</a>
+            <a href="#"><img style="width: 24px;height: 24px;" src="./img/facebook.png" alt=""></a>
+            <a href="#"><img style="width: 24px;height: 24px;" src="./img/instagram.png" alt=""></a>
+            <a href="#"><img style="width: 24px;height: 24px;" src="./img/line.png" alt=""></a>
+        </div>
+    </footer>
 </body>
 
 </html>
